27e57b42cc18b6dd97781bc32105ac471dd6954c
Garik <ural24.03.2019@gmail.com>
2025-01-19 10:43:18 +0300

    Initial Commit

---
+++ create_bot.py
@@ -0,0 +1,23 @@
+import logging
+from aiogram import Bot, Dispatcer
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import PaseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from decouple import config
+from apscheduler.schendulers.asyncio import AsyncIOScheduler
+
+#from db_handler.db_class import PostgresHandler
+from handlers.start  import start_router
+
+
+#pg_db = PostgresHandler(config('PG_LINK'))
+scheduler = AsyncIOScheduler(timzone='Europe/Moskow')
+admins = [int(admin_id) for admin_id in config('ADMINS').split(',')]
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+loger = logging.getLogger(__name__)
+
+bot = Bot(token=config('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage())
+
+dp.include_router(start_router)
---
+++ handlers/start.py
@@ -0,0 +1,17 @@
+from aiogram import Router, F
+from aiogram.filters import Commands, Command
+from aiogram.types import Message
+
+start_router = Router()
+
+@start_router.message(CommandStart())
+async def cmd_start(message: Message):
+	await message.answer('Starting message on call /start using the CommandStart() filter')
+	
+@start_router.message(Command('start_2'))
+async def cmd_start_2(message: Message):
+	await message.answer('Starting message on call /start_2 using the CommandStart() filter')
+
+@start_router.message(F.text == '/start_3')
+async def cmd_start_3(message: Message):
+	await message.answer('Starting message on call /start_3 using the F.text  filter')
