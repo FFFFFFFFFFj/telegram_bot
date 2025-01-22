f0ef0df9ee53b4a8706bdf64ba2d8f3dfbbefea5
Garik <ural24.03.2019@gmail.com>
2025-01-22 16:24:17 +0300

    Added h1 tag

--- aiogram_run.py
+++ aiogram_run.py
@@ -2,7 +2,7 @@ import asyncio
 from create_bot import bot, dp,scheduler
 from handlers.start import start_router
 #from work_time.time_func import send_time_msg
-
+#test
 async def main():
 	#schenduler.add_job(send_time_msg, 'interval', second=10)
 	#schenduler.start()
